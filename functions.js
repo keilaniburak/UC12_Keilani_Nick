85ed9347ab67bfecfb8c2c07c520d9a5521abdc8
Keilani K Burak <kb17180@srcs.loc>
2017-03-01 08:09:26 -0800

    display area code function added

--- functions.js
+++ functions.js
@@ -40,4 +40,24 @@ function getAreaCode(phoneNum) {
         return undefined;
     }
 }
-S
+
+/**
+ * Displays the area code for an inputted phone number
+ * @param {string} inputId  The element id for the text box
+ * @param {string} outputId The element id of message div
+ */
+function displayAreaCode(inputId, outputId) {
+    var outputText = "";
+    var phoneNum = document.getElementById(inputId).value;
+
+    // Now try to get the code
+    try {
+        var areaCode = getAreaCode(phoneNum);
+        outputText = "Your area code is " + areaCode;
+    } catch (error) {
+        console.log(error.message);
+        outputText = error.message;
+    }
+
+    document.getElementById(outputId).innerHTML = outputText;
+}
