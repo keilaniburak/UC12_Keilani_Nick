1860b0c9916f7219dfdcc47f9f3ff406411a3257
Keilani K Burak <kb17180@srcs.loc>
2017-03-03 09:01:50 -0800

    Tests for user story 3 & 4

--- tests.js
+++ tests.js
@@ -13,3 +13,30 @@ QUnit.test( "Errors thrown for getAreaCode", function( assert ) {
         getAreCode("(415 444-5555");
     }, "Missing ')'. An error should have been thrown." );
 });
+
+QUnit.test("Test the getCoCode function.", function (assert) {
+    var num = "(415) 555-5555";
+    var result = getCoCode(num);
+    assert.deepEqual(result, " 555", "Valid CO code test passed.");
+});
+
+QUnit.test( "Errors thrown for getCoCode", function( assert ) {
+    assert.throws( function() {
+        getCoCode("415)444-5555");
+    }, "Missing ')'. An error should have been thrown." );
+
+    assert.throws( function() {
+        getAreCode("(415 444-5555");
+    }, "Missing '-'. An error should have been thrown." );
+});
+
+QUnit.test("Test the getLineCode function.", function (assert) {
+    var num = "(415) 555-5555";
+    var result = getLineCode(num);
+    assert.deepEqual(result, "5555", "Valid line code test passed.");
+});
+
+
+
+
+
