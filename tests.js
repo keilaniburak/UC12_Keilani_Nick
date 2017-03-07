60f624403e14d8d04ed4dae03594a52f57ba1871
Keilani K Burak <kb17180@srcs.loc>
2017-03-07 08:03:19 -0800

    Merging Code

--- tests.js
+++ tests.js
@@ -4,7 +4,7 @@ QUnit.test("Test the getAreaCode function.", function (assert) {
     assert.deepEqual(result, "415", "Valid area code test passed.");
 });
 
-QUnit.test( "Errors thrown for getAreaCode", function( assert ) {
+QUnit.test( "Errors thrown for getAreaCode", function(assert) {
     assert.throws( function() {
         getAreCode("415)444-5555");
     }, "Missing '('. An error should have been thrown." );
@@ -36,7 +36,8 @@ QUnit.test("Test the getLineCode function.", function (assert) {
     assert.deepEqual(result, "5555", "Valid line code test passed.");
 });
 
-
-
-
-
+QUnit.test("Test the validPhoneNumber function.", function (assert) {
+    var num = "(415) 555-5555";
+    var result = validPhoneNumber(num);
+    assert.deepEqual(result, true, "Valid Phone Number test passed.");
+});
